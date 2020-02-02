4ac089712db5e6956d9726959f29c4fa711f0e3c
laravel-Almighty <almighty.developers.1726@gmail.com>
2020-02-03 00:36:49 +0530

    Add model for primary car and card

--- app/Profile.php
+++ app/Profile.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     //
+
+    public function PrimaryCar()
+    {
+        return $this->hasOne('App\MyCar','user_id' , 'user_id')->where('primary',true);
+    }
+
+    public function PrimaryCard()
+    {
+        return $this->hasOne('App\PaymentCard','user_id' , 'user_id')->where('primary',true);
+    }
 }
