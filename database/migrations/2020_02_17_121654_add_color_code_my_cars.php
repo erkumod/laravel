2384881a1b28f79cc94f5a1896502e684beedae6
laravel-Almighty <almighty.developers.1726@gmail.com>
2020-02-17 18:16:28 +0530

    changes for color_name in my_cars

--- database/migrations/2020_02_17_121654_add_color_code_my_cars.php
+++ database/migrations/2020_02_17_121654_add_color_code_my_cars.php
@@ -14,7 +14,6 @@ class AddColorCodeMyCars extends Migration
     public function up()
     {
         Schema::table('my_cars', function (Blueprint $table) {
-            $table->string('color_name')->nullable()->default(null);
             $table->string('color_code')->nullable()->default(null);
         });
     }
