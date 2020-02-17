879838ec6acbe26cc50aa312c4290e965f0582f1
laravel-Almighty <almighty.developers.1726@gmail.com>
2020-02-17 17:47:56 +0530

    Minor changes for migration

--- database/migrations/2020_02_17_121654_add_color_code_my_cars.php
+++ database/migrations/2020_02_17_121654_add_color_code_my_cars.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColorNameMyCars extends Migration
+class AddColorCodeMyCars extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddColorNameMyCars extends Migration
     public function up()
     {
         Schema::table('my_cars', function (Blueprint $table) {
-            $table->string('color_name')->nullable()->default(null);
+            $table->string('color_code')->nullable()->default(null);
         });
     }
 
@@ -25,8 +25,6 @@ class AddColorNameMyCars extends Migration
      */
     public function down()
     {
-        Schema::table('my_cars', function (Blueprint $table) {
-            $table->dropColumn('color_name');
-        });
+        $table->dropColumn('color_code');
     }
 }
