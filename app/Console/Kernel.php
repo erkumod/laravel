96cee11c1867f1d05dbd9740e200dafa374066a7
hiren-joyride <hiren@joyride.city>
2020-04-02 21:31:56 +0530

    Changes for cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('completewash:notification')->everyMinute();
+        $schedule->command('completewash:notification')->everyFifteenMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }
